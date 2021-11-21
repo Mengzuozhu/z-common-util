9f6875e4193f797f3b2c05e3964d97c09438899d
Mengzuozhu <mengzuozhu@foxmail.com>
2021-11-21 16:43:44 +0800

    ObjenesisStd线程安全

--- common-util/src/main/java/com/github/mzz/common/bean/BeanCreator.java
+++ common-util/src/main/java/com/github/mzz/common/bean/BeanCreator.java
@@ -9,7 +9,7 @@ import org.objenesis.instantiator.ObjectInstantiator;
  * @author mengzz
  */
 public class BeanCreator {
-    private static ThreadLocal<ObjenesisStd> objenesisStdThreadLocal = ThreadLocal.withInitial(ObjenesisStd::new);
+    private final static ObjenesisStd OBJENESIS_STD = new ObjenesisStd();
 
     /**
      * New instance.
@@ -19,7 +19,7 @@ public class BeanCreator {
      * @return the t
      */
     public static <T> T newInstance(Class<T> clazz) {
-        return objenesisStdThreadLocal.get().newInstance(clazz);
+        return OBJENESIS_STD.newInstance(clazz);
     }
 
     /**
@@ -30,6 +30,6 @@ public class BeanCreator {
      * @return the instantiator of
      */
     public static <T> ObjectInstantiator<T> getInstantiatorOf(Class<T> clazz) {
-        return objenesisStdThreadLocal.get().getInstantiatorOf(clazz);
+        return OBJENESIS_STD.getInstantiatorOf(clazz);
     }
 }
