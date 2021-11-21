230792eb16a1616bda6abd5029d79867f9008622
Mengzuozhu <mengzuozhu@foxmail.com>
2021-11-21 18:35:21 +0800

    修复：FileUtilTest找不到目录

--- common-util/src/test/java/com/github/mzz/common/file/FileUtilTest.java
+++ common-util/src/test/java/com/github/mzz/common/file/FileUtilTest.java
@@ -1,6 +1,7 @@
 package com.github.mzz.common.file;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -14,13 +15,18 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author mengzz
  */
 public class FileUtilTest {
-    private String jsonFile = "src/test/resources/demo.json";
-    private String jsonFile2 = "src/test/resources/demo2.json";
-    private String outFile = "src/test/resources/out/test.zip";
 
     @Test
     public void zip() throws IOException {
+        String jsonFile = "src/test/resources/demo.json";
+        String jsonFile2 = "src/test/resources/demo2.json";
+        String outDir = "src/test/resources/out";
+        FileUtils.forceMkdir(new File(outDir));
+        String outFile = outDir + "/test.zip";
+
         FileUtil.zip(Lists.newArrayList(jsonFile, jsonFile2), outFile);
-        assertTrue(new File(outFile).exists());
+        File result = new File(outFile);
+        assertTrue(result.exists());
+        assertTrue(result.delete());
     }
 }
